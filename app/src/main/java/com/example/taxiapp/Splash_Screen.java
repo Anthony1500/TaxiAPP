24725af0cd2b1117c9848a97de29f72c0232607d
Anthony <arteaga17000@gmail.com>
2022-06-08 18:36:38 -0500

    v0.2

---
+++ app/src/main/java/com/example/taxiapp/Splash_Screen.java
@@ -0,0 +1,30 @@
+package com.example.taxiapp;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+    private final int DURACION_SPLASH =4000;// tiempo de espera
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {// metodo para pasar de una pantalla a otra
+                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+
+
+}
