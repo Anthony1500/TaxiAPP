93987c6be7ff13850c3e9807554fc81290083cb6
dodybryan <johnparedes209@gmail.com>
2022-06-08 20:52:41 -0500

    loginv1

---
+++ app/src/main/java/com/example/taxiapp/login.java
@@ -0,0 +1,12 @@
+package com.example.taxiapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class login extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+
+}
