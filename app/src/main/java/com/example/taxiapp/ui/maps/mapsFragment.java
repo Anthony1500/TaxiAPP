97cc5471c48c1766ce4298040bf1838a4f9faaa6
Anthony <arteaga17000@gmail.com>
2022-06-30 19:38:08 -0500

    v0.11

--- app/src/main/java/com/example/taxiapp/ui/maps/mapsFragment.java
+++ app/src/main/java/com/example/taxiapp/ui/maps/mapsFragment.java
@@ -7,30 +7,50 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.taxiapp.databinding.FragmentMapsBinding;
+import com.example.taxiapp.R;
 
-public class mapsFragment extends Fragment {
-    private FragmentMapsBinding binding;
+import com.example.taxiapp.databinding.FragmentHomeBinding;
+import com.example.taxiapp.databinding.FragmentMapsBinding;
+import com.example.taxiapp.ui.home.HomeViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapsFragment  extends Fragment implements OnMapReadyCallback{
+    private GoogleMap mMap;
+    //private ActivityMapsBinding binding;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mapsViewModel maViewModel =
-                new ViewModelProvider(this).get(mapsViewModel.class);
 
-        binding = FragmentMapsBinding.inflate(inflater, container, false);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
 
 
         return root;
     }
 
+
+
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
 
